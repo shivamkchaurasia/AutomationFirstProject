634de6da2c77585aa0517f17a2ad764919a71d1e
shivam <shivam.taskmanager@gmail.com>
2022-08-23 02:06:54 +0530

    First Commit

---
+++ AutomationFirstProject/src/main/java/AutomationFirstProject/AutomationFirstProject/App.java
@@ -0,0 +1,8 @@
+package AutomationFirstProject.AutomationFirstProject;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
